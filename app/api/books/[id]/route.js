454e24cc99eeda4a409f49d5d259b3605a92a750
Oskar Kupski <oskar.kupski@gmail.com>
2024-04-14 16:50:40 +0200

    added endpoint for upadting book

--- app/api/books/[id]/route.js
+++ app/api/books/[id]/route.js
@@ -10,3 +10,31 @@ export const DELETE = async (request, { params }) => {
 
     return new Response();
 }
+
+export const PATCH = async (request, { params }) => {
+    const id = params.id;
+    const book = await request.json();
+
+    const bookDetailsQuery = await sql`
+        SELECT * FROM books
+        WHERE id = ${id};
+    `;
+    const [bookDetails] = bookDetailsQuery.rows;
+    
+    await sql`
+        UPDATE books SET 
+        title = ${book.title || bookDetails.title},
+        author = ${book.author || bookDetails.author},
+        pinned = ${book.pinned === 'undefined' ?  bookDetails.pinned : book.pinned}
+        WHERE id = ${id};
+    `;
+
+    const updatedBookQuery = await sql`
+        SELECT * FROM books
+        WHERE id = ${id};
+    `;
+
+    const [updatedBook] = updatedBookQuery.rows;
+
+    return Response.json(updatedBook);
+}
